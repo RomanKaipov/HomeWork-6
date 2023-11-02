919805eb2ec1c0eb064d08384b9d4780b063e5b9
Roman <roman2012-56rus@yandex.ru>
2023-11-02 20:33:41 +0300

    Add homework

---
+++ Task 1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    int number;
+    number = int.Parse(a);
+    return number;
+}
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = ReadInt($"Enter number {i + 1}: ");
+    }
+    return array;
+}
+
+int CountPositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void printArray(int[] array)
+{
+    Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    Console.WriteLine();
+}
+
+int length = ReadInt("Enter length array:");
+int[] array = CreateArray(length);
+printArray(array);
+Console.WriteLine($"Count positive number {CountPositiveNumber(array)}");
---
+++ Task 2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    double number;
+    number = double.Parse(a);
+    return number;
+}
+
+double[] Line(int numberLine)
+{
+    double[] line = new double[2];
+    line[0] = ReadInt($"Введите значение k{numberLine}");
+    line[1] = ReadInt($"Введите значение b{numberLine}");
+    return line;
+}
+
+double[] FindCoord(double[] lineOne, double[] lineTwo)
+{
+    double[] coord = new double[2];
+    coord[0] = (lineOne[1] - lineTwo[1]) / (lineTwo[0] - lineOne[0]);
+    coord[1] = lineOne[0]*coord[0]+lineOne[1];
+    return coord;
+}
+
+bool CheckLine(double[] lineOne, double[] lineTwo)
+{
+    if (lineTwo[0] == lineOne[0])
+    {
+        if (lineTwo[1] == lineOne[1])
+        {
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+double[] lineOne = Line(1);
+double[] lineTwo = Line(2);
+
+if (CheckLine(lineOne,lineTwo))
+{
+double[] coord = FindCoord(lineOne,lineTwo);
+Console.WriteLine($"Координата пересечения равна ({coord[0]},{coord[1]})");
+}
